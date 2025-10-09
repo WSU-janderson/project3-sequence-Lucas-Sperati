34010d062b755ddf29cbd828e99ee53858a9010e
Lucas-Sperati <sperati.2@wright.edu>
2025-10-09 16:21:16 -0400

    started implementing class constructors and started methods

---
+++ Sequence.cpp
@@ -0,0 +1,6 @@
+#include "Sequence.h"
+#include <iostream>
+
+using namespace std;
+
+//Sequence::Sequence(size_t sz = 0)
--- Sequence.h
+++ Sequence.h
@@ -1,3 +1,30 @@
+#include <string>
+using namespace std;
+
+class Sequence {
+public: //variables maybe
+    size_t sz; // size_t == unsigned long long
+
+public://methods maybe
+    //default constructor
+    Sequence();
+
+    //parameterized constructor
+    Sequence(size_t sz);
+
+    //TODO no idea if I need this. Just following
+    //linked list example.
+    //This all makes a deep copy of the Sequence
+    //copy constructor - can make deep copy
+    Sequence(const Sequence& other);
+    //assignment operator
+    Sequence& operator=(const Sequence& other);
+    //deconstructor
+    ~Sequence();
+
+
+};
+
 class SequenceNode {
     public: //public so you don't need getters or setters
 
